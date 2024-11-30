9b41f1c750ef6e8cb7740fb13a9d5b5a640a026a
Beelzenord <beelzenord@gmail.com>
2024-11-30 22:18:13 +0100

    Mostly template pages so far

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator,NativeStackScreenProps } from '@react-navigation/native-stack'; // {{ edit_1 }}
+import { RootStackParamList } from './src/util/types';
+import HomeScreen from './src/util/components/HomeScreen/homescreen.component';
+import Settings from './src/util/components/settingsScreen/settings.component';
+import Profile from './src/util/components/ProfileScreen/profile.component';
+import CameraScreen from './src/util/components/CameraScreen/camera.component';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Settings" component={Settings} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Camera" component={CameraScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
